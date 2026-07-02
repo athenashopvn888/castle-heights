50cdf76c607ae1bfd7c7a7cbe9e2e2da7e5df329
athenashopvn888 <athenashopvn888@gmail.com>
2026-07-03 02:57:21 +0700

    fix: align blog canonical and sitemap domain

--- app/robots.ts
+++ app/robots.ts
@@ -7,6 +7,6 @@ export default function robots(): MetadataRoute.Robots {
       allow: "/",
       disallow: ["/api/"],
     },
-    sitemap: "https://castleheightscannabis.com/sitemap.xml",
+    sitemap: "https://castleheightscannabis.ca/sitemap.xml",
   };
 }
